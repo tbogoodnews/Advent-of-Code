e08d30989f8f79623524f089a31032bec93a60da
Goodernews <40648132+Goodernews@users.noreply.github.com>
2023-12-03 21:45:03 -0800

    Cleaned solution

--- 2023/Day-4/solution.py
+++ 2023/Day-4/solution.py
@@ -2,39 +2,16 @@ input_txt = open("input.txt")
 
 data = [ l.split(": ")[1] for l in input_txt.read()[:-1].split("\n")]
 data = [[v.split(" ")  for v in l.split(" | ")] for l in data]
-
+matches = [ sum(n in [int(i) for i in l[1] if i != ""] for n in [int(i) for i in l[0] if i != ""]) for l in data ]
 
 def part_one(d):
-    total = 0
-    for l in d:
-        nums = [int(i) for i in l[0] if i != ""]
-        winning = [int(i) for i in l[1] if i != ""]
-        temp = 0
-        for n in nums:
-            if n in winning:
-                if temp == 0:
-                    temp = 1
-                else:
-                    temp = temp * 2
-        total += temp
-    return total
+    return sum( 2**(x - 1) for x in d if x != 0) 
 
 def part_two(d):
-    total = 0
     won_cards = list(range(1, len(d)+1))
     for x in range(1, 1 + len(d)):
-        l = d[x-1]
-        nums = [int(i) for i in l[0] if i != ""]
-        winning = [int(i) for i in l[1] if i != ""]
-        temp = 0
-        for n in nums:
-            if n in winning:
-                temp += 1
-        prev_seen = won_cards.count(x)
-        won_cards += prev_seen * list( range (x+1, x + 1 + temp))
+        won_cards += won_cards.count(x) * list( range (x+1, x + 1 + d[x-1]))
     return len(won_cards)
 
-
-
-print("Solution to part one:", part_one(data))
-print("Solution to part two:", part_two(data))
+print("Solution to part one:", part_one(matches))
+print("Solution to part two:", part_two(matches))
