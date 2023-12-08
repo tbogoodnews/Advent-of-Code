992ec77c6acd4d34b145631c3e72f64260a197c5
Goodernews <40648132+Goodernews@users.noreply.github.com>
2023-12-07 22:23:58 -0800

    Need to fix

--- 2023/Day-8/pypy_solution.py
+++ 2023/Day-8/pypy_solution.py
@@ -1,11 +1,9 @@
-import functools
 input_txt = open("input.txt")
 
 data = input_txt.read()[:-1].split("\n")
 moves = data[0]
 nodes = {l.split(" ")[0] : {"L" : l[-9:-6], "R" : l[-4:-1] } for l in data[2:]}
 
-@functools.cache
 def move_step(current_node, direction):
     return nodes[current_node][direction]
 
@@ -23,10 +21,9 @@ def part_two():
     curr_nodes = [x for x in nodes.keys() if x[2] == "A"] # end in A
     while not all(x[2] == "Z" for x in curr_nodes):
         direction = moves[steps % len(moves)]
-        curr_nodes = set(move_step(i, direction) for i in curr_nodes)
+        curr_nodes = [move_step(i, direction) for i in curr_nodes]
         steps += 1
     return steps
 
-
-print("Solution to part one:", part_one())
+#print("Solution to part one:", part_one())
 print("Solution to part two:", part_two())
